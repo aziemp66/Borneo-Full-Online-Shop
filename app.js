014546b895bcbd99e325323035d4424bc9772347
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-29 21:32:07 +0700

    Adding Session Configuration

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const csrf = require("csurf");
+const expressSession = require("express-session");
 
 const db = require("./data/database");
 const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const authRoutes = require("./routes/auth.routes");
+const createSessionConfig = require("./config/session");
 
 const app = express();
 
@@ -16,6 +18,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 
 app.use(addCsrfTokenMiddleware);
---
+++ config/session.js
@@ -0,0 +1,27 @@
+const expressSession = require("express-session");
+const mongoDbStore = require("connect-mongodb-session");
+
+function createSessionStore() {
+    const MongoDbStore = mongoDbStore(expressSession);
+    const store = new MongoDbStore({
+        uri: "mongodb://localhost:27017",
+        databaseName: "borneo-online-shop",
+        collection: "sessions",
+    });
+
+    return store;
+}
+
+function createSessionConfig() {
+    return {
+        secret: "super-secret",
+        resave: false,
+        saveUninitialized: false,
+        store: createSessionStore(),
+        cookie: {
+            maxAge: 60 * 60 * 1000,
+        },
+    };
+}
+
+module.exports = createSessionConfig;
