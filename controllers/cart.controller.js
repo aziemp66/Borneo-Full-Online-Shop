981582b2ec2862197bc015096ff9686354116dac
Azie Melza Pratama <aziemp66@gmail.com>
2022-02-21 00:16:13 +0700

    Working on the Shopping Cart Logic

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,5 +1,22 @@
-function addCartItem(req, res) {
-    res.locals.card.addItem();
+const Product = require("../models/product.model");
+
+async function addCartItem(req, res, next) {
+    let product;
+    try {
+        product = await Product.findById(req.body.productId);
+    } catch (error) {
+        next(error);
+        return;
+    }
+    const cart = res.locals.cart;
+
+    cart.addItem(product);
+    req.session.cart = cart;
+
+    res.status(201).json({
+        message: "Product added to cart!",
+        newTotalItems: cart.totalQuantity,
+    });
 }
 
 module.exports = { addCartItem };
--- models/cart.model.js
+++ models/cart.model.js
@@ -1,6 +1,8 @@
 class Cart {
-    constructor(items = []) {
+    constructor(items = [], totalQuantity = 0, totalPrice = 0) {
         this.items = items;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
     }
 
     addItem(product) {
@@ -16,9 +18,16 @@ class Cart {
                 cartItem.quantity = cartItem.quantity + 1;
                 cartItem.totalPrice = cartItem.totalPrice + product.price;
                 this.items[i] = cartItem;
-            } else {
-                this.items.push(cartItem);
+
+                this.totalQuantity = this.totalQuantity + 1;
+                this.totalPrice = this.totalPrice + product.price;
+                return;
             }
         }
+        this.items.push(cartItem);
+        this.totalQuantity = this.totalQuantity + 1;
+        this.totalPrice = this.totalPrice + product.price;
     }
 }
+
+module.exports = Cart;
