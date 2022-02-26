ccbd4c9861a79d05472f91c82d44a447c20fa84c
Azie Melza Pratama <aziemp66@gmail.com>
2022-02-26 12:26:16 +0700

    Completed Stripe Checkouts

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -36,22 +36,21 @@ async function addOrder(req, res, next) {
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
-        line_items: [
-            {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        line_items: cart.items.map((item) => {
+            return {
                 price_data: {
                     currency: "usd",
                     product_data: {
-                        name: "Dummy",
+                        name: item.product.title,
                     },
-                    unit_amount_decimal: 10.99,
+                    unit_amount_decimal: +item.product.price.toFixed(2) * 100,
                 },
-                quantity: 1,
-            },
-        ],
+                quantity: item.quantity,
+            };
+        }),
         mode: "payment",
-        success_url: `localhost:3000/orders/success`,
-        cancel_url: `localhost:3000/orders/failure`,
+        success_url: `http://localhost:3000/orders/success`,
+        cancel_url: `http://localhost:3000/orders/failure`,
     });
 
     res.redirect(303, session.url);
